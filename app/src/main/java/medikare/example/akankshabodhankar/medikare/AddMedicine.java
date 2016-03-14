99d004853fdeb5d80ed67b95ae150b8a7c818a5d
AkankshaBodhankar <kmit.akankshabodhankar@gmail.com>
2016-03-15 02:24:07 +0530

    Added listview functionality

--- app/src/main/java/medikare/example/akankshabodhankar/medikare/AddMedicine.java
+++ app/src/main/java/medikare/example/akankshabodhankar/medikare/AddMedicine.java
@@ -1,36 +1,45 @@
 package medikare.example.akankshabodhankar.medikare;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.app.TimePickerDialog;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by akanksha bodhankar on 20-12-2015.
  */
 public class AddMedicine extends Activity {
 
+
+    public ArrayList results = new ArrayList();
+    private String tableName = dbhelp.tableName;
+    static SQLiteDatabase db= null;
     EditText name_et,dosage_et,purpose_et,id_et;
+    ListView lv;
     Button submit_bt;
     TextView timepicker_tv;
-    String id;
-    String name;
-    String purpose;
-    String time;
-    String dosage;
     Calendar calendar = Calendar.getInstance();
     static final int dialog_id=0;//to get appropriate dialog
     int hour=0,minute=0;
@@ -40,32 +49,43 @@ public class AddMedicine extends Activity {
         setContentView(R.layout.addmedicine);
 
 
+
+        dbhelp dbh = new dbhelp(this,"kmit.db",null,1);
+        db = dbh.getWritableDatabase();
+
+
         timepicker_tv = (TextView)findViewById(R.id.timepicker_tv);
         submit_bt = (Button)findViewById(R.id.submit_bt);
         name_et = (EditText)findViewById(R.id.name_et);
         dosage_et = (EditText)findViewById(R.id.dosage_et);
         purpose_et = (EditText)findViewById(R.id.purpose_et);
-        id_et = (EditText)findViewById(R.id.id_et);
+
+
         showTimePickerDialog();
 
 
-/*
         submit_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 insert();
-               // OnClick_Add(v);
+
+
+                // show();
+
 
             }
-        });*/
+        });
     }
 
-  /* private void insert() {
 
-        name = name_et.getText().toString();
-        time = String.valueOf(hour+minute);
-        dosage=dosage_et.getText().toString();
-        purpose= purpose_et.getText().toString();
+
+  private void insert() {
+
+       String name = name_et.getText().toString();
+        String time = String.valueOf(hour + minute);
+        int dosage = Integer.parseInt(dosage_et.getText().toString());
+        String purpose= purpose_et.getText().toString();
 
 
         ContentValues cv = new ContentValues();
@@ -73,24 +93,28 @@ public class AddMedicine extends Activity {
         cv.put("time",time);
         cv.put("dosage", dosage);
         cv.put("purpose", purpose);
-        long r = db.insert("medicines",null,cv);
+        long r = db.insert("medicine",null,cv);
 
         if(r==-1)
             Toast.makeText(getApplicationContext(), "Text not inserted", Toast.LENGTH_LONG).show();
-        else
-            Toast.makeText(getApplicationContext(),"Text inserted",Toast.LENGTH_LONG).show();
+        else {
+            Toast.makeText(getApplicationContext(), "Text inserted", Toast.LENGTH_LONG).show();
+
+            Log.d("ff",results.toString());
+        }
 
 
-    }*/
-    public void showTimePickerDialog()
-    {
-        // bt_submit = (Button)findViewById(R.id.picktime_button);
-        timepicker_tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new TimePickerDialog(AddMedicine.this, onTimeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show();
-            }
-        });
+    }
+
+
+  public void showTimePickerDialog() {
+      // bt_submit = (Button)findViewById(R.id.picktime_button);
+      timepicker_tv.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              new TimePickerDialog(AddMedicine.this, onTimeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show();
+          }
+      });
     }
     TimePickerDialog.OnTimeSetListener onTimeSetListener=
             new TimePickerDialog.OnTimeSetListener() {
@@ -102,16 +126,41 @@ public class AddMedicine extends Activity {
 
                 }
             };
-    public void saveData(View view)
-    {
-        id = id_et.getText().toString();
-        name = name_et.getText().toString();
-        time = String.valueOf(hour + minute);
-        dosage=dosage_et.getText().toString();
-        purpose= purpose_et.getText().toString();
-        BackgroundTask backgroundTask = new BackgroundTask(this);
-        backgroundTask.execute("add_info",id,name,time,dosage,purpose);
-        finish();
+
+   public void openAndQueryDatabase() {
+        try {
+
+            Cursor c = db.rawQuery("SELECT id, name, time FROM " +
+                    tableName, null);
+
+            if (c != null ) {
+                if (c.moveToFirst()) {
+                    do {
+
+                        int id = c.getInt(c.getColumnIndex("id"));
+                        String Name = c.getString(c.getColumnIndex("name"));
+                        String time = c.getString(c.getColumnIndex("time"));
+                      //  int dosage = c.getInt(c.getColumnIndex("dosage"));
+                        results.add("ID "+ id +"Name: " + Name + "Time: " + time);
+                    }while (c.moveToNext());
+                }
+            }
+        } catch (SQLiteException se ) {
+            Log.e(getClass().getSimpleName(), "Could not create or Open the database");
+        } finally {
+            if (db != null)
+                db.execSQL("DELETE FROM " + tableName);
+            db.close();
+        }
+
+
     }
+
+    public ArrayList get()
+    {
+        return results;
+    }
+
+
 
 }
--- app/src/main/java/medikare/example/akankshabodhankar/medikare/MainActivity.java
+++ app/src/main/java/medikare/example/akankshabodhankar/medikare/MainActivity.java
@@ -3,10 +3,12 @@ package medikare.example.akankshabodhankar.medikare;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.ListActivity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -17,28 +19,41 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TabHost;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
     TextView addmedicine_tv;
+    private ArrayList mymed = new ArrayList();
+   static SQLiteDatabase db= null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BackgroundTask backgroundTask = new BackgroundTask(this);
-        backgroundTask.execute("get_info");
+
 
         addmedicine_tv = (TextView) findViewById(R.id.addmedicine_tv);
+        dbhelp dbh = new dbhelp(this,"kmit.db",null,1);
+        db = dbh.getWritableDatabase();
+        //show();
+        displayResultList();
+
 
         addmedicine_tv.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +64,87 @@ public class MainActivity extends Activity {
             }
         });
 
+    }
+
+   /*public  void show()
+    {
+        Cursor result = db.rawQuery("select *from medicine", null);
+        TableLayout tb = (TableLayout)findViewById(R.id.table);
+        tb.removeAllViews();
+        if(result.moveToFirst())  // true if only first row exists
+        {
+            do {
+                TableRow tr = new TableRow(getApplicationContext());
+                TextView t1 = new TextView(getApplicationContext());
+                TextView t2 = new TextView(getApplicationContext());
+                TextView t3 = new TextView(getApplicationContext());
+                int id = result.getInt(0);
+                String name = result.getString(1);
+                String  time = result.getString(2);
+                t1.setText(id+"   ");
+                t2.setText(name+"   ");
+                t3.setText(time + "   ");
+                t1.setTextColor(Color.BLACK);
+                t2.setTextColor(Color.BLACK);
+                t3.setTextColor(Color.BLACK);
+                t1.setTextSize(20);
+                t2.setTextSize(20);
+                t3.setTextSize(20);
+                tr.addView(t1);
+                tr.addView(t2);
+                tr.addView(t3);
+                tb.addView(tr);
+                tb.setBackgroundColor(Color.GRAY);
+
+
+            }
+            while(result.moveToNext());
+
+        }
 
     }
+*/
+    private void displayResultList() {
+
+       /* TextView tView = new TextView(this);
+        tView.setText("This data is retrieved from the database and only 4 " +
+                "of the results are displayed");
+        ListView lv = (ListView)findViewById(R.id.list);
+
+        lv.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, AddMedicine.results));
+
+        lv.setTextFilterEnabled(true);*/
+
+        Cursor result = db.rawQuery("select *from medicine", null);
+
+        if(result.moveToFirst())  // true if only first row exists
+        {
+            do {
+
+                int id = result.getInt(0);
+                String name = result.getString(1);
+                String  time = result.getString(2);
+                String row = String.valueOf(id)+"\n"+name+" "+time;
+                mymed.add(row);
+
+
+            }
+            while(result.moveToNext());
+
+        }
+
+
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mymed));
+        getListView().setTextFilterEnabled(true);
+
+
+
+
+    }
+
+
+
+
 }
